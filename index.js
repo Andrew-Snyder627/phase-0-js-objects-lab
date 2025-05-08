659b66283d0107cac63e8025cc6340ca16aefba3
Andrew Snyder <andrew.snyder9702@gmail.com>
2025-05-07 22:56:10 -0400

    saving work for submission

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
 //Write your code here
+const attendee = {
+  attendeeId: "T001",
+  name: "Alice Smith",
+  event: "JavaScript Conference",
+  ticketType: "VIP",
+  ticketPrice: 150.00
+}
 
+function logAttendeeName(object) {
+  console.log(object.name);
+}
+
+function logTicketPrice(object) {
+  console.log(object.ticketPrice);
+}
+
+function updateTicketType(object, type) {
+  object.ticketType = type;
+}
+
+function updateTicketPrice(object, price) {
+  object.ticketPrice = price;
+}
+
+function removeEventProperty(object) {
+  delete object.event;
+}
+
+function addCheckedInProperty(object) {
+  object.checkedIn = true;
+}
 
 
 //Needed for the tests to work. Don't modify
